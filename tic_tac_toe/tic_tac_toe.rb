df908e4fc6b17a9902d9b3d2c592ee7fb5b7ca82
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2020-12-10 00:37:13 -0800

    All basic game functions completed

--- tic_tac_toe/tic_tac_toe.rb
+++ tic_tac_toe/tic_tac_toe.rb
@@ -5,12 +5,11 @@ module Validate
   def number?(obj)
     obj = obj.to_s unless obj.is_a? String
     x = /\A[+-]?\d+(\.\d+)?\z/.match(obj)
-    # sets input numbers outside 1-9 to nil tp reject
-    if obj.to_i < 1 || obj.to_i > 9
-      x = nil
-    end
-    return obj.to_i unless x.nil? 
-      return x
+    # sets input numbers outside 1-9 to nil to reject
+    x = nil if obj.to_i < 1 || obj.to_i > 9
+    return obj.to_i unless x.nil?
+
+    x
   end
 end
 
@@ -20,7 +19,6 @@ class Player
   def initialize(name, symbol)
     @name = name
     @symbol = symbol
-    @victory = false
   end
 
   def turn
@@ -32,13 +30,10 @@ class Player
       puts "I'm sorry. That's an invalid entry. Please try again."
       x = self.turn
     end
-    return x
+    x
   end
 
-  def check_winner
-    @victory == true
-  end
-  attr_reader :name, :symbol, :victory
+  attr_reader :name, :symbol
 end
 
 # Pictoral represenation of tic-tac-toe board
@@ -46,7 +41,7 @@ class Board
   # if any one of these variables is true the relevant space ((0-8) + 1) is occupied
   def initialize
     @occupied = []
-    for i in 0..8
+    (0..8).each do |_i|
       @occupied.push(false)
       @one_row = '   |   |   '
       @two_row = +' 1 | 2 | 3 '
@@ -59,45 +54,53 @@ class Board
       @nine_row = '   |   |   '
     end
   end
-  
-  def change(num, symb) 
+
+  def stalemate
+    unless @occupied.include?(false)
+      puts "Looks like a stalemate. Please, play again."
+      exit!
+    end
+  end
+
+  def change(num, symb)
     # converts user input to array counting style
     num = (num - 1)
     if @occupied[num] == false
       @occupied[num] = true
       alter_board(num, symb)
     else
-      puts "That selection (#{(num + 1)}) is not available. Please try again."
-      return false
+      puts "That selection (#{num + 1}) is not available. Please try again."
+      false
     end
   end
 
   def alter_board(num, symb)
-    if num == 0
+    case num
+    when 0
       @two_row[1] = symb
-    elsif num == 1
+    when 1
       @two_row[5] = symb
-    elsif num == 2
+    when 2
       @two_row[9] = symb
-    elsif num == 3
+    when 3
       @five_row[1] = symb
-    elsif num == 4
+    when 4
       @five_row[5] = symb
-    elsif num == 5
+    when 5
       @five_row[9] = symb
-    elsif num == 6
+    when 6
       @eight_row[1] = symb
-    elsif num == 7
+    when 7
       @eight_row[5] = symb
-    elsif num == 8
+    when 8
       @eight_row[9] = symb
-    
+
     end
   end
 
   def display
     puts @one_row
-    puts @two_row 
+    puts @two_row
     puts @three_row
     puts @four_row
     puts @five_row
@@ -107,16 +110,91 @@ class Board
     puts @nine_row
   end
 
+  # checks all 8 possible win variations for indentical characters
+  def check_winner(symb, name)
+    counter = 0
+    win_var_one = []
+    win_var_one.push(@two_row[1], @two_row[5], @two_row[9])
+    win_var_one.each do |x|
+      counter += 1 if x == symb
+    end
+    winner(counter, name)
 
+    counter = 0
+    win_var_two = []
+    win_var_two.push(@five_row[1], @five_row[5], @five_row[9])
+    win_var_two.each do |x|
+      counter += 1 if x == symb
+    end
+    winner(counter, name)
 
+    counter = 0
+    win_var_three = []
+    win_var_three.push(@eight_row[1], @eight_row[5], @eight_row[9])
+    win_var_three.each do |x|
+      counter += 1 if x == symb
+    end
+    winner(counter, name)
+
+    counter = 0
+    win_var_four = []
+    win_var_four.push(@two_row[1], @five_row[1], @eight_row[1])
+    win_var_four.each do |x|
+      counter += 1 if x == symb
+    end
+    winner(counter, name)
+
+    counter = 0
+    win_var_five = []
+    win_var_five.push(@two_row[5], @five_row[5], @eight_row[5])
+    win_var_five.each do |x|
+      counter += 1 if x == symb
+    end
+    winner(counter, name)
+
+    counter = 0
+    win_var_six = []
+    win_var_six.push(@two_row[9], @five_row[9], @eight_row[9])
+    win_var_six.each do |x|
+      counter += 1 if x == symb
+    end
+    winner(counter, name)
+
+    counter = 0
+    win_var_seven = []
+    win_var_seven.push(@two_row[1], @five_row[5], @eight_row[9])
+    win_var_seven.each do |x|
+      counter += 1 if x == symb
+    end
+    winner(counter, name)
+
+    counter = 0
+    win_var_eight = []
+    win_var_eight.push(@two_row[9], @five_row[5], @eight_row[1])
+    win_var_eight.each do |x|
+      counter += 1 if x == symb
+    end
+    winner(counter, name)
+
+    false
+  end
+
+  def winner(counter, name)
+    name = name.upcase
+    if counter == 3
+      puts "CONGRATULATIONS #{name}. YOU WIN!"
+      display
+      exit!
+    end
+  end
 end
 
 puts 'Player One: Enter your name'
-name = gets.chomp
-player_one = Player.new(name, 'X')
+name_one = gets.chomp
+player_one = Player.new(name_one, 'X')
 puts 'Player Two: Enter your name'
-name = gets.chomp
-player_two = Player.new(name, 'O')
+name_two = gets.chomp
+player_two = Player.new(name_two, 'O')
 
 board = Board.new
 
@@ -127,19 +205,21 @@ while victory == false
   checker = false
   if counter.odd?
     while checker == false
+      board.stalemate
       board.display
       turn = player_one.turn
       symb = player_one.symbol
       checker = board.change(turn, symb)
     end
-    victory = player_one.check_winner
+    victory = board.check_winner('X', name_one)
   else
     while checker == false
+      board.stalemate
       board.display
       turn = player_two.turn
       symb = player_two.symbol
       checker = board.change(turn, symb)
     end
-    victory = player_two.check_winner
+    victory = board.check_winner('O', name_two)
   end
 end
