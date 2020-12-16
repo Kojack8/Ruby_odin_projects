b956b39f4bb1c2762e9205b5a04c6b49cc6de94e
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2020-12-16 03:11:26 -0800

    NPC Guess works correctlygit add -Agit add -A Still needs scoring and more work on game modes.

--- mastermind/game_run.rb
+++ mastermind/game_run.rb
@@ -4,12 +4,14 @@ require_relative 'user_input'
 require_relative 'compare_answer'
 
 class GameRun
-  include UserInput, CompareAnswer
+  include CompareAnswer
+  include UserInput
   def initialize(game_mode, key)
     p game_mode
     p key
     @player_one_score = 0
     @player_two_score = 0
+    @confirmed_wrong_loc = {}
   end
 
   def human_guess(key, guess_counter, player, player_points)
@@ -26,7 +28,7 @@ class GameRun
     puts '| 4 - WHITE   |'
     puts '| 5 - YELLOW  |'
     puts '|_____________|'
-    puts "Guess ##{(guess_counter + 1)}/12 for Agent #{player}. Using the numbers below enter your four guesses" \
+    puts "Guess ##{guess_counter + 1}/12 for Agent #{player}. Using the numbers below enter your four guesses" \
     ' one at a time.'
     answer = take_input
     if answer == key
@@ -34,94 +36,114 @@ class GameRun
       return true
     end
     @comparison = compare_answer(key, answer)
-    puts "WHAT FOLLOWS SHOULD BE THE COMPARISON ARRAY"
+    puts 'WHAT FOLLOWS SHOULD BE THE COMPARISON ARRAY'
     p @comparison
-    return false
+    false
   end
+
   def round_end
     puts 'YOU HAVE SUCCESSFULLY CRACKED THE CODE'
   end
 
-
-  public
   # npc starts by guess 4 colors at random
   # it memorizes correct (RED) comparisons and will attempt to reuse white elsewhere
   def npc_guess(key, guess_counter, player_points)
-  p "KEY" + key.to_s
+    p "KEY#{key}"
     if player_points == 1
-    @player_two_score += 1
-  else
-    @player_one_score += 1
-  end
-  puts "This is Agent Ruby's guess ##{(guess_counter + 1)}/12."
-  sleep(2)
-  puts "Agent Ruby is finalizing their guess."
-  # THIS PART DETERMINES WHETHER TO START FROM A BLANK ARRAY OR USE SOMETHING PASSED IN
-  if guess_counter == 0
-    colors = [1, 2, 3, 4, 5]
-    @answer = []
-    (0..3).each do |_i|
-      @answer.push(colors[rand(5)])
+      @player_two_score += 1
+    else
+      @player_one_score += 1
     end
-    @comparison = compare_answer(key, @answer)
-    p "NPC GUESS = " + @answer.to_s
-    p "Comparison generated for NPC (NEEDED FOR LIGHTS FOR USERS SATISFACTION!!!!!"
-    p @comparison
-  else
-    counter = 0
-    confirmed_wrong_loc = {}
-    confirmed_wrong_colors = []
-    p "ANSWER PHASE TWO"
-    p @answer
-    @answer.map! { |x|
-      
-      if @comparison[counter] == 2
-        x = x
+    puts "This is Agent Ruby's guess ##{guess_counter + 1}/12."
+    sleep(2)
+    puts 'Agent Ruby is finalizing their guess.'
+    # THIS PART DETERMINES WHETHER TO START FROM A BLANK ARRAY OR USE SOMETHING PASSED IN
+    if guess_counter.zero?
+      colors = [1, 2, 3, 4, 5]
+      @answer = []
+      (0..3).each do |_i|
+        @answer.push(colors[rand(5)])
+      end
+      p "NPC ANSWER#{@answer}"
+      @comparison = compare_answer(key, @answer)
+      p "COMPARISON ONE = #{@comparison}"
+    else
+      counter = -1
+      confirmed_wrong_colors = []
+      @answer.map! do |x|
+        counter += 1
+        case @comparison[counter]
+        when 2
+          x = x
 
-      elsif @comparison[counter] == 1
-        # key is the space-number and the values are an array which contains the colors
-        # that the npc should not guess at the keys location again
-        begin 
-          confirmed_wrong_loc[counter] << x
-        rescue 
-          confirmed_wrong_loc[counter] = [x]
+        when 1
+          # key is the space-number and the values are an array which contains the colors
+          # that the npc should not guess at the keys location again
+          begin
+            @confirmed_wrong_loc[counter] << x
+          rescue StandardError
+            @confirmed_wrong_loc[counter] = [x]
+          end
+          x = intel_guess(confirmed_wrong_colors, counter)
+
+          # if at the location of this comparison the value is nil
+          # that means the color and location is wrong
+          # so the color in the answer at the same location can be added to the wrong
+          # array
+        else
+          confirmed_wrong_colors.push(x)
+          x = intel_guess(confirmed_wrong_colors, counter)
+
         end
-        x = intel_guess(confirmed_wrong_loc, confirmed_wrong_colors, counter)
-
-
-        # if at the location of this comparison the value is nil
-        # that means the color and location is wrong
-        # so the color in the answer at the same location can be added to the wrong
-        # array
-      else
-        confirmed_wrong_colors.push(x)
-        x = intel_guess(confirmed_wrong_loc, confirmed_wrong_colors, counter)
-     
       end
-      counter += 1
-    }
-  end
-  false
+      p 'NPC ANSWER ' + @answer.to_s
+      @comparison = compare_answer(key, @answer)
+      p @comparison
+      if @answer == key
+        round_end
+        return true
+      end
+    end
+    false
   end
 
-  # THIS IS WHERE I LEFT OFF. NONE OF THIS ALL WORKS EXACTLY RIGHT
-  # ALL OF NPC TURN MIGHT NEED TO BE REWORKED
-  def intel_guess(confirmed_wrong_loc, confirmed_wrong_colors, counter)
+  # NPC maintains an array of possible answers
+  # Any number that is determined to definetly not be in the key is sliced out
+  def intel_guess(confirmed_wrong_colors, counter)
     possible_answers = [1, 2, 3, 4, 5]
-    p "CONFIRMED WRONG COLORS" + confirmed_wrong_colors.to_s
-    possible_answers.each do |x|  
-      if confirmed_wrong_colors.include?(x)
-        possible_answers.slice!(possible_answers.index(x))
+    possible_answers.each do |x|
+      possible_answers.slice!(possible_answers.index(x)) if confirmed_wrong_colors.include?(x)
+    end
+    final = guess_possibles(possible_answers, counter)
+  end
+
+  # Takes the array of possible answers minus answers we know won't be included
+  def guess_possibles(possible_answers, counter)
+    # Generates one random number from that array
+    answer = possible_answers[rand(0..(possible_answers.length - 1))]
+    if @confirmed_wrong_loc.key?(counter)
+      # Checks the WHITE PEG hash to see if this color in this location is a repeat
+      if @confirmed_wrong_loc[counter].include?(answer)
+        # if it is, just rerun the whole thing
+        answer = guess_possibles(possible_answers, counter)
+      else
+        # stores answer not to be repeated
+        begin
+          @confirmed_wrong_loc[counter] << answer
+        rescue StandardError
+          @confirmed_wrong_loc[counter] = [answer]
+        end
       end
     end
-      # If the hash contains the key which represents the space-order in the array
-    p "CONFIRMED WRONG LOC"
-    p confirmed_wrong_loc
-    if confirmed_wrong_loc.key?(counter)
-      puts confirmed_wrong_loc[counter]
+    # stores answer not to be repeated
+    begin
+      @confirmed_wrong_loc[counter] << answer
+    rescue StandardError
+      @confirmed_wrong_loc[counter] = [answer]
     end
-    p "POSSIBLE ANSWERS" + possible_answers.to_s
-    return puts possible_answers[rand(1..possible_answers.length)]
+    
+    return answer
   end
+
   attr_reader :player_one_score, :player_two_score
 end
