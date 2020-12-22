242244ae622b79ae21b800060a194986ef2ebdd3
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2020-12-22 02:34:18 -0800

    Knuth progress. EoD B/u

--- mastermind/knuth/knuth_comparison.rb
+++ mastermind/knuth/knuth_comparison.rb
@@ -1,2 +1,54 @@
-require_relative 'compare_answer'
+require_relative './../compare_answer'
+
+class KnuthComparison
+  def run_comparison(key, knu_arr)
+    comparison_arr = []
+    # creates a massive array that is made up of 1296 key-answer comparison arrays
+    knu_arr.each { |x|
+      compare = CompareAnswer.new(key, x)
+      comparison = compare.run_comparison
+      comparison_arr << comparison
+    }
+    comparison_arr
+  end
+
+  def remove_comparisons(knu_arr, comparison_arr, comparison)
+    counter = 0
+    comparison_arr.each { |x| 
+      if x != comparison
+        knu_arr.slice!(counter)
+        comparison_arr.slice!(counter)
+        counter -= 1
+      else
+        counter += 1
+      end
+    }
+    knu_arr
+  end
+
+  def default_possibilities
+    comp_possibilities = {
+      [2, 2, 2, 2] => 0, [2, 2, 2, 0] => 0, [2, 2, 0, 0] => 0, 
+      [2, 0, 0, 0] => 0, [0, 0, 0, 0] => 0, [2, 2, 1, 1] => 0,
+      [2, 2, 1, 0] => 0, [2, 1, 1, 0] => 0, [2, 1, 0, 0] => 0,
+      [1, 1, 0, 0] => 0, [1, 0, 0, 0] => 0, [1, 1, 1, 1] => 0, 
+      [1, 1, 1, 0] => 0, [2, 1, 1, 1] => 0
+      }
+      
+  end
+
+  def calc_possibilities(comparison, poss_arr)
+    p poss_arr
+    poss_arr.map { |k, v| 
+      if k == comparison
+        poss_arr[k] += 1
+      end
+      
+    }
+    return poss_arr
+
+
+
+  end
+end
 
--- mastermind/npc_guess.rb
+++ mastermind/npc_guess.rb
@@ -2,6 +2,7 @@
 require_relative 'compare_answer'
 require_relative 'check_winner'
 require_relative './knuth/knuth_initial'
+require_relative './knuth/knuth_comparison'
 
 class NPCGuess
   include CheckWinner
@@ -20,17 +21,48 @@ class NPCGuess
       puts "This is Agent Ruby's guess ##{@guess_counter + 1}/12."
       sleep(2)
       puts 'Agent Ruby is finalizing their guess.'
+      # Creates an array containing all 1296 possible guesses
+      knuth = KnuthInitial.new
+      knu_arr = knuth.generator
       
       if @guess_counter.zero?
         @guess_counter += 1
-        compare = CompareAnswer.new(@key, [1, 1, 2, 2])
+        guess = [1, 1, 2, 2]
+        compare = CompareAnswer.new(@key, guess)
         @comparison = compare.run_comparison
-        p "COMPARE = " + @comparison.to_s
-      else
-        knuth = KnuthInitial.new
-        knu_arr = knuth.generator
-        p knu_arr
-        p knu_arr.length
+        knu_arr.slice!(knu_arr.index(guess))
+        knuthcomparison = KnuthComparison.new
+
+        comparison_arr = knuthcomparison.run_comparison(@key, knu_arr)
+        # knu_arr_two contains the all the arrays that match the comparison return
+        knu_arr_two = knuthcomparison.remove_comparisons(knu_arr, comparison_arr, @comparison)
+
+        p @comparison
+      elsif @guess_counter == 1
+        @possibilities_arr = knuthcomparison.default_possibilities
+        @g_score_arr = []
+        knu_arr.each { |g| 
+          g.each{ |x|
+            compare = CompareAnswer.new(x, g)
+            compare = compare.run_comparison
+            @possibilities_arr = knuthcomparison.calc_possibilities(compare, @possibilities_arr)
+            # this is the least informative possible result of guessing g
+            g_score = @possibilities_arr.sort_by { |k, v| v }.last
+            p "G SCORE " + g_score.to_s
+            
+          }
+          # THIS IS ON THE TRAIL OF SOMETHING BUT G SCORE IS NOT YET CORRECT
+          
+          @possibilities_arr = knuthcomparison.default_possibilities
+          p @g_score_arr
+        }
+       
+
+        
+
+
+
+        
 
 
 
@@ -44,7 +76,6 @@ class NPCGuess
       
 
       
-        
     end
     @guess_counter
   end
