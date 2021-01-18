18b218f1c7a6f7bf36689eb48e7b2cfb9ec0a7ff
Kojack8 <72641704+Kojack8@users.noreply.github.com>
2021-01-17 17:09:54 -0800

    Load implementation

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -5,12 +5,25 @@ require_relative 'select_key'
 require_relative 'display'
 require_relative 'game_run'
 
+def new_game
+  select_key = SelectKey.new
+  select_key.dict_to_arr
+  key = select_key.select_key
+  game = GameRun.new(key)
+  game.game_run
+end
+
 load = LoadGame.new
-load.load_game
+if load.load_game == true
+  new_game
+else
+  unless load.load_sequence == false
 
-select_key = SelectKey.new
-select_key.dict_to_arr
-key = select_key.select_key
+  else
+    new_game
+  end
 
-game = GameRun.new(key)
-game.game_run
+
+end
+
+
--- hangman/lib/load_game.rb
+++ hangman/lib/load_game.rb
@@ -1,20 +1,61 @@
+# frozen_string_literal: true
+require 'yaml'
+
+# allows the user to load a previously saved game or start a new one
 class LoadGame
   def load_game
-    puts "If you'd like to start a new game enter \"1\" now."
-    puts "Or if you'd like to load a saved game enter \"2\"."
+    load_text
     correct = false
     while correct == false
       x = gets.chomp
       correct = validate(x)
     end
+    x.to_i == 1
   end
 
-  def validate(x)
-    if x.to_i < 1 || x.to_i > 2 
+  def load_text
+    puts "If you'd like to start a new game enter \"1\" now."
+    puts "Or if you'd like to load a saved game enter \"2\"."
+  end
+
+  def validate(choice)
+    if choice.to_i < 1 || choice.to_i > 2
       puts 'I\'m sorry. That\'s an invalid input. Please try again.'
       false
     else
       true
     end
+  end
+
+  def load_sequence 
+    load_choices
+    unless user_entry == false
+      file = File.open(@saved_files[@save_index])
+      YAML.dump(file)
+    else 
+      false
+    end
+  end
+  def load_choices
+    puts "Select a file from below to load or type exit to start a new game."
+    @saved_files = Dir["./../saved/**/*.yaml"]
+    @available_files = Dir["./../saved/**/*.yaml"]
+    puts @available_files.each { |n| n.slice!(0..10)}
+  end
+  def user_entry
+    validator = false
+    while validator == false
+      input = gets.chomp
+      if @available_files.include?(input)
+        validator = true
+        @save_index = @available_files.index(input)
+      elsif input.downcase == "exit"
+        return false
+      else
+        puts "I'm sorry. That's an invalid entry. Please try again minding " \
+        "capitalization of filenames or type exit to start a new game."
+      end
+    end
+    input
   end
 end
